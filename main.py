a660998d80ccdfd75b69fc3ad68bb78f82295754
Shruhrid Banthia <shrurhid.banthia@iitgn.ac.in>
2022-12-28 02:39:23 +0530

    Added usage, added changes for optimization

--- main.py
+++ main.py
@@ -3,38 +3,73 @@ import io
 from fastapi import FastAPI
 from fastapi.responses import FileResponse, StreamingResponse
 from pydantic import BaseModel
-
-from ml import obtain_image
+from enum import Enum
+from ml import obtain_image, clf, iris
 
 app = FastAPI()
-
+#### Inital exploration of the FastAPI framework ####
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"message": "Hello World"}
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+# We can use our own class using enum to which we want the input to belong to:
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+# We can use the same class to validate the input:
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+# We can extract and return data from a database:
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
+### Using FastAPI for Machine Learning ###
+# We can use FastAPI to create a machine learning API
 
-class Item(BaseModel):
-    name: str
-    price: float
-    tags: list[str] = []
+# This is how we validatae the input data
+class request_body(BaseModel):
+    sepal_length : float
+    sepal_width : float
+    petal_length : float
+    petal_width : float
 
+# We predict(through the post method) the class of the flower based on the input data and return the class
+@app.post("/predict")
+def predict(data : request_body):
+    test_data = [[
+            data.sepal_length, 
+            data.sepal_width, 
+            data.petal_length, 
+            data.petal_width
+    ]]
+    class_idx = clf.predict(test_data)[0]
+    return { 'class' : iris.target_names[class_idx]}
 
-@app.post("/items/")
-def create_item(item: Item):
-    return item
-
+### Using FastAPI for Image Generation using stable diffusion ###
 
 @app.get("/generate")
 def generate_image(
     prompt: str,
     *,
-    seed: int | None = None,
+    seed: int = 1024,
     num_inference_steps: int = 50,
     guidance_scale: float = 7.5
 ):
@@ -52,7 +87,7 @@ def generate_image(
 def generate_image_memory(
     prompt: str,
     *,
-    seed: int | None = None,
+    seed: int = 1024,
     num_inference_steps: int = 50,
     guidance_scale: float = 7.5
 ):
@@ -66,3 +101,6 @@ def generate_image_memory(
     image.save(memory_stream, format="PNG")
     memory_stream.seek(0)
     return StreamingResponse(memory_stream, media_type="image/png")
+
+
+    
--- ml.py
+++ ml.py
@@ -1,36 +1,45 @@
 from pathlib import Path
-
 import torch
 from diffusers import StableDiffusionPipeline
 from PIL.Image import Image
+from sklearn.datasets import load_iris
+from sklearn.naive_bayes import GaussianNB
 
+### FOr a simple model with input data as a post request from the frontend###
+
+# Loading Iris Dataset
+iris = load_iris()
+
+# Getting features and targets from the dataset
+X = iris.data
+Y = iris.target
+
+# Fitting our Model on the dataset
+clf = GaussianNB()
+clf.fit(X,Y)
+
+### For using model from huggingface hub ###
 token_path = Path("token.txt")
 token = token_path.read_text().strip()
 
 # get your token at https://huggingface.co/settings/tokens
 pipe = StableDiffusionPipeline.from_pretrained(
     "CompVis/stable-diffusion-v1-4",
-    revision="fp16",
-    torch_dtype=torch.float16,
+    #revision="fp16",
+    #torch_dtype=torch.float16,
     use_auth_token=token,
 )
-
-pipe.to("cuda")
-
-# prompt = "a photograph of an astronaut riding a horse"
-
-
-# image = pipe(prompt)["sample"][0]
-
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+pipe.to(device)
 
 def obtain_image(
     prompt: str,
     *,
-    seed: int | None = None,
+    seed: int = 1024,
     num_inference_steps: int = 50,
     guidance_scale: float = 7.5,
 ) -> Image:
-    generator = None if seed is None else torch.Generator("cuda").manual_seed(seed)
+    generator = None if seed is None else torch.Generator(device).manual_seed(seed)
     print(f"Using device: {pipe.device}")
     image: Image = pipe(
         prompt,
@@ -40,5 +49,3 @@ def obtain_image(
     ).images[0]
     return image
 
-
-# image = obtain_image(prompt, num_inference_steps=5, seed=1024)
